e29951902f5cdaca927be59fa71cb14cfbe4f94e
Dsouza <Dsouza@10.0.0.226>
2022-09-28 00:34:26 -0400

    Created JFrame, Jpanel for create and retrieve employees

---
+++ src/model/Employee.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Dsouza
+ */
+public class Employee {
+    private String Name;
+    private int EmployeeID;
+    private int Age;
+    private String Gender;
+    private String Startdate;
+    private String Level;
+    
+    private String Positiontitle;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public int getEmployeeID() {
+        return EmployeeID;
+    }
+
+    public void setEmployeeID(int EmployeeID) {
+        this.EmployeeID = EmployeeID;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int Age) {
+        this.Age = Age;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+
+    public String getStartdate() {
+        return Startdate;
+    }
+
+    public void setStartdate(String Startdate) {
+        this.Startdate = Startdate;
+    }
+
+    public String getLevel() {
+        return Level;
+    }
+
+    public void setLevel(String Level) {
+        this.Level = Level;
+    }
+
+    public String getPositiontitle() {
+        return Positiontitle;
+    }
+
+    public void setPositiontitle(String Positiontitle) {
+        this.Positiontitle = Positiontitle;
+    }
+    
+    
+            
+}
